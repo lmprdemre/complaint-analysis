8e41efa2f119804a8360161a8f1608f7a8ef2a1d
Emre Şahin <emresahin639@gmail.com>
2018-06-11 18:29:40 +0300

    Test

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -2,6 +2,8 @@
 <!-- Left Sidebar - style you can find in sidebar.scss  -->
 <!-- ============================================================== -->
 <div class="navbar-default sidebar" role="navigation">
+
+
     <div class="sidebar-nav slimscrollsidebar">
         <div class="sidebar-head">
             <h3><span class="fa-fw open-close"><i class="ti-close ti-menu"></i></span> <span class="hide-menu">Navigation</span></h3> </div>
